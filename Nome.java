d4cb056c2f59c62cb0733e294dbf834cb35529af
Simone D'elia <s.delia91@gmail.com>
2019-11-19 11:02:36 +0100

    aggiunta classe nome.java

---
+++ Nome.java
@@ -0,0 +1,15 @@
+package ProvaSerale;
+
+public class Cognome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String nome = "Simone";
+		String cognome = "D'Elia";
+		
+		System.out.println(nome + " " +cognome);
+
+	}
+
+}
