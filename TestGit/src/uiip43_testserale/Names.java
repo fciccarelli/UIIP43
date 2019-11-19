ab5ca1e9748cf40e710f18f6b51253460e39be13
luigiscialpi <luigi_scialpi@hotmail.it>
2019-11-19 23:36:59 +0100

    Modifica ordine nomi

--- TestGit/src/uiip43_testserale/Names.java
+++ TestGit/src/uiip43_testserale/Names.java
@@ -3,12 +3,12 @@ package uiip43_testserale;
 public class Names {
 
 	public static void main(String[] args) {
-	
+		
+		System.out.println("Pasquale Oppido");
 		System.out.println("Assaiante Fabio");
 		System.out.println("Simona Branca");
 		System.out.println("Simone D'Elia");
 		System.out.println("Antonietta De Angelis");
-		System.out.println("Pasquale Oppido");
 		System.out.println("Luigi Scialpi");
 	}
 
