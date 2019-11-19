597ac405c1813cd256e0504cf5dfca130be8c88d
SimonaBranca <brancasimona94@gmail.com>
2019-11-19 11:04:32 +0100

    modificato il nome della classe Branca.java

---
+++ Simona.java
@@ -0,0 +1,15 @@
+package it.uiip.Cognome;
+
+public class Branca {
+
+	public static void main(String[] args) {
+		
+		String nome = "Simona";
+		String cognome = "Branca";
+		
+		System.out.println(nome +" "+ cognome);
+		
+
+	}
+
+}
