b100ed4dafd10fc6596b4f1c9bc44e09fd4bcda6
Antonietta867 <antoniettadeangelis878@gmail.com>
2019-11-22 01:41:48 +0100

    aggunto tre query alle precedenti

--- W1D4 – AntoniettaDeangelis/query.sql
+++ W1D4 – AntoniettaDeangelis/query.sql
@@ -1,18 +1,39 @@
 #select citta from aeroporto
 #where npiste = null;
 
-#select a.nazione from Aeroporto as a
+#1 select a.nazione from Aeroporto as a
 #left join Volo as v on (v.aeroportoPart = a.id ) and (v.aeroportoArr = a.id)
 #where  idVolo = '1';
-use viaggi;
-
-select a.tipoAereo from Aereo as a 
-left join volo as v 
-on v.aeroportoPart= a.tipoAereo
-left join aeroporto as ar
-on v.aeroportoArr=ar.id
-where ar.citta='roma';
+#use viaggi;
 
 
+
+
+#2 select distinct a.tipoAereo  from Aereo as a 
+#left join volo as v 
+#on v.tipoAereo = a.tipoAereo
+#left join aeroporto as ar
+#on v.aeroportoPart=ar.id
+#where ar.citta='Roma';
+
+#3 select distinct a.tipoAereo ,  a.npass from Aereo as a 
+#left join volo as v 
+#on v.tipoAereo = a.tipoAereo
+#left join aeroporto as ar
+#on v.aeroportoPart=ar.id
+#where ar.citta='Roma';
+
+#4 select distinct a.tipoAereo ,  a.npass from Aereo as a 
+#left join volo as v 
+#on v.tipoAereo = a.tipoAereo
+#left join aeroporto as ar
+#on v.aeroportoPart=ar.id
+#where ar.citta='Roma';
+
+#5 select distinct ar.citta from aeroporto as ar
+#left join volo as v 
+#on  v.aeroportoPart=ar.id
+#where v.aeroportoArr='AMS'
+#order by citta asc;
 
 
