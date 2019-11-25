27658e2aad21a256677cc4a19539c55c993ec676
Antonietta867 <antoniettadeangelis878@gmail.com>
2019-11-26 00:02:04 +0100

    aggiunta cartella custom di de angelis

---
+++ custom/airport/airportbackoffice/gensrc/org/airportbackoffice/constants/GeneratedYacceleratorbackofficeConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 25-nov-2019 15.32.36                        ---
+ * ----------------------------------------------------------------
+ */
+package org.airportbackoffice.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedYacceleratorbackofficeConstants
+{
+	public static final String EXTENSIONNAME = "airportbackoffice";
+	
+	protected GeneratedYacceleratorbackofficeConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
