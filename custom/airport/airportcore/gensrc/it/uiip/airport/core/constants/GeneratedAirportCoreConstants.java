fdb208a279f1bc6c596437604209d88db4d86e53
Simone D'elia <s.delia91@gmail.com>
2019-11-25 23:51:44 +0100

    aggiunta cartella custom

---
+++ custom/airport/airportcore/gensrc/it/uiip/airport/core/constants/GeneratedAirportCoreConstants.java
@@ -0,0 +1,63 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 25-nov-2019 23.11.47                        ---
+ * ----------------------------------------------------------------
+ */
+package it.uiip.airport.core.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedAirportCoreConstants
+{
+	public static final String EXTENSIONNAME = "airportcore";
+	public static class TC
+	{
+		public static final String AIRPORT = "Airport".intern();
+		public static final String APPARELPRODUCT = "ApparelProduct".intern();
+		public static final String APPARELSIZEVARIANTPRODUCT = "ApparelSizeVariantProduct".intern();
+		public static final String APPARELSTYLEVARIANTPRODUCT = "ApparelStyleVariantProduct".intern();
+		public static final String ELECTRONICSCOLORVARIANTPRODUCT = "ElectronicsColorVariantProduct".intern();
+		public static final String FLIGHT = "Flight".intern();
+		public static final String PLANE = "Plane".intern();
+		public static final String SWATCHCOLORENUM = "SwatchColorEnum".intern();
+	}
+	public static class Attributes
+	{
+		// no constants defined.
+	}
+	public static class Enumerations
+	{
+		public static class SwatchColorEnum
+		{
+			public static final String BLACK = "BLACK".intern();
+			public static final String BLUE = "BLUE".intern();
+			public static final String BROWN = "BROWN".intern();
+			public static final String GREEN = "GREEN".intern();
+			public static final String GREY = "GREY".intern();
+			public static final String ORANGE = "ORANGE".intern();
+			public static final String PINK = "PINK".intern();
+			public static final String PURPLE = "PURPLE".intern();
+			public static final String RED = "RED".intern();
+			public static final String SILVER = "SILVER".intern();
+			public static final String WHITE = "WHITE".intern();
+			public static final String YELLOW = "YELLOW".intern();
+		}
+	}
+	public static class Relations
+	{
+		public static final String AIRPORTARRIVAL2FLIGHTRELATION = "AirportArrival2FlightRelation".intern();
+		public static final String DEPARTUREAIRPORT2FLIGHTRELATION = "DepartureAirport2FlightRelation".intern();
+		public static final String PLANE2FLIGHTRELATION = "Plane2FlightRelation".intern();
+	}
+	
+	protected GeneratedAirportCoreConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
