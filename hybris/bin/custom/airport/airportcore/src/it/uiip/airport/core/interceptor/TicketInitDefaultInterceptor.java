d7077205955dbb54e6c8191f50e2262040d3b5a4
Simone D'elia <s.delia91@gmail.com>
2019-12-02 23:42:26 +0100

    modified

--- hybris/bin/custom/airport/airportcore/src/it/uiip/airport/core/interceptor/TicketInitDefaultInterceptor.java
+++ hybris/bin/custom/airport/airportcore/src/it/uiip/airport/core/interceptor/TicketInitDefaultInterceptor.java
@@ -3,9 +3,9 @@
  */
 package it.uiip.airport.core.interceptor;
 
+import de.hybris.platform.servicelayer.interceptor.InitDefaultsInterceptor;
 import de.hybris.platform.servicelayer.interceptor.InterceptorContext;
 import de.hybris.platform.servicelayer.interceptor.InterceptorException;
-import de.hybris.platform.servicelayer.interceptor.PrepareInterceptor;
 
 import it.uiip.airport.core.model.TicketModel;
 
@@ -14,11 +14,11 @@ import it.uiip.airport.core.model.TicketModel;
  * @author sdeli
  *
  */
-public class TicketPrepareInterceptor implements PrepareInterceptor<TicketModel>
+public class TicketInitDefaultInterceptor implements InitDefaultsInterceptor<TicketModel>
 {
 
 	@Override
-	public void onPrepare(final TicketModel arg0, final InterceptorContext arg1) throws InterceptorException
+	public void onInitDefaults(final TicketModel arg0, final InterceptorContext arg1) throws InterceptorException
 	{
 		// XXX Auto-generated method stub
 
--- hybris/bin/custom/airport/airportcore/src/it/uiip/airport/core/interceptor/TicketRemoveInterceptor.java
+++ hybris/bin/custom/airport/airportcore/src/it/uiip/airport/core/interceptor/TicketRemoveInterceptor.java
@@ -1,13 +1,27 @@
 /**
- * 
+ *
  */
 package it.uiip.airport.core.interceptor;
+
+import de.hybris.platform.servicelayer.interceptor.InterceptorContext;
+import de.hybris.platform.servicelayer.interceptor.InterceptorException;
+import de.hybris.platform.servicelayer.interceptor.RemoveInterceptor;
+
+import it.uiip.airport.core.model.TicketModel;
+
 
 /**
  * @author sdeli
  *
  */
-public class TicketRemoveInterceptor
+public class TicketRemoveInterceptor implements RemoveInterceptor<TicketModel>
 {
+
+	@Override
+	public void onRemove(final TicketModel arg0, final InterceptorContext arg1) throws InterceptorException
+	{
+		// XXX Auto-generated method stub
+
+	}
 
 }
